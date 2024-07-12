6b7342a36d7a5345660aa5fdda6efeb9ad283b9d
hotzings <hotzings@gmail.com>
2024-07-11 20:24:31 -0400

    added mods

---
+++ UpdateServer.bat
@@ -0,0 +1,13 @@
+xcopy mods C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods /s /e
+xcopy config C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\config /s /e
+xcopy global_packs C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\global_packs /s /e
+xcopy kubejs C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\kubejs /s /e
+
+
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\Essential-forge_1-20-1.jar
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\LegendaryTooltips-1.20.1-forge-1.4.5.jar
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\embeddium-0.3.24+mc1.20.1.jar
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\entityculling-forge-1.6.6-mc1.20.1.jar
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\LegendaryTooltips-1.20.1-forge-1.4.5.jar
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\oculus-mc1.20.1-1.7.0.jar
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\rubidium-extra-0.5.4.3+mc1.20.1-build.121.jar
