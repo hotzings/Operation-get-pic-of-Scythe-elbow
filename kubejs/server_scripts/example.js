42f527d8a81156407437578d12daceaac532092a
hotzings <hotzings@gmail.com>
2024-07-12 23:47:01 -0400

    added default keybinds added temp recipe for orb of origin

--- kubejs/server_scripts/example.js
+++ kubejs/server_scripts/example.js
@@ -4,3 +4,27 @@
 
 console.info('Hello, World! (Loaded server scripts)')
 
+// Listen for the "recipes" server event.
+ServerEvents.recipes(event => {
+    // You can replace `event` with any name you like, as
+    // long as you change it inside the callback too!
+  
+    // This part, inside the curly braces, is the callback.
+    // You can modify as many recipes as you like in here,
+    // without needing to use ServerEvents.recipes() again.
+
+    event.shaped(
+        Item.of('origins:orb_of_origin', 1), // arg 1: output
+        [
+          'ded',
+          'ede', // arg 2: the shape (array of strings)
+          'ded'
+        ],
+        {
+          d: 'minecraft:diamond',
+          e: 'minecraft:emerald'
+        }
+      )
+  
+    console.log('Hello! The recipe event has fired!')
+  })
