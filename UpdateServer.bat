cf828e9943e1bd1aa94c4cd9cc60966b048e4109
hotzings <hotzings@gmail.com>
2024-07-13 20:04:45 -0400

    added quite flags to UpdateServer.bat organized kubejs recipes a bit

--- UpdateServer.bat
+++ UpdateServer.bat
@@ -1,15 +1,20 @@
-del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods
-
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods /q
 xcopy mods C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods /s /e
+
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\config /q
 xcopy config C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\config /s /e
+
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\global_packs /q
 xcopy global_packs C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\global_packs /s /e
+
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\kubejs /q
 xcopy kubejs C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\kubejs /s /e
 
 
-del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\Essential-forge_1-20-1.jar
-del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\LegendaryTooltips-1.20.1-forge-1.4.5.jar
-del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\embeddium-0.3.24+mc1.20.1.jar
-del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\entityculling-forge-1.6.6-mc1.20.1.jar
-del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\LegendaryTooltips-1.20.1-forge-1.4.5.jar
-del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\oculus-mc1.20.1-1.7.0.jar
-del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\rubidium-extra-0.5.4.3+mc1.20.1-build.121.jar
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\Essential-forge_1-20-1.jar /q
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\LegendaryTooltips-1.20.1-forge-1.4.5.jar /q
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\embeddium-0.3.24+mc1.20.1.jar /q
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\entityculling-forge-1.6.6-mc1.20.1.jar /q
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\LegendaryTooltips-1.20.1-forge-1.4.5.jar /q
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\oculus-mc1.20.1-1.7.0.jar /q
+del C:\Users\zings\AppData\Roaming\ATLauncher\servers\ScorchPackServer\mods\rubidium-extra-0.5.4.3+mc1.20.1-build.121.jar /q
--- kubejs/server_scripts/Recipes/Botania.js
+++ kubejs/server_scripts/Recipes/Botania.js
@@ -2,8 +2,6 @@
 
 // Visit the wiki for more info - https://kubejs.com/
 
-console.info('Hello, World! (Loaded server scripts)')
-
 // Listen for the "recipes" server event.
 ServerEvents.recipes(event => {
     // You can replace `event` with any name you like, as
@@ -13,19 +11,10 @@ ServerEvents.recipes(event => {
     // You can modify as many recipes as you like in here,
     // without needing to use ServerEvents.recipes() again.
 
-
-
-    // this needs to be changed im thinking a loot table
-    event.shaped(
-        Item.of('origins:orb_of_origin', 1), // arg 1: output
-        [
-          'ded',
-          'ede', // arg 2: the shape (array of strings)
-          'ded'
-        ],
-        {
-          d: 'minecraft:diamond',
-          e: 'minecraft:emerald'
-        }
-      )
+    // Add shapeless crafting recipe for botania:red_string
+    event.shapeless("botania:red_string", [
+      ['2x #chipped:redstone_block'],
+      ['2x minecraft:string'],
+      ['2x botania:pixie_dust']
+    ]);
   })
